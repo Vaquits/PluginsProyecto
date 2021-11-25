5e061a4f3c87b4e2df39a3d7c4a442348e0501f4
Vaquits <80922879+Vaquits@users.noreply.github.com>
2021-11-25 16:24:56 -0500

    Delete primer-plugin directory

--- primer-plugin/plugin.min.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";tinymce.PluginManager.add("primer-plugin",function(n,t){n.ui.registry.addButton("primer-plugin",{text:"primer-plugin button",onAction:function(){"Hola"==n.getContent()&&n.setContent("Si")}})})}();
