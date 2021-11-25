26d272738cfba2c71879daec919f7e7862c19a0d
Vaquits <80922879+Vaquits@users.noreply.github.com>
2021-11-25 00:03:05 -0500

    Add files via upload

---
+++ primer-plugin/plugin.min.js
@@ -0,0 +1,1 @@
+!function(){"use strict";tinymce.PluginManager.add("primer-plugin",function(n,t){n.ui.registry.addButton("primer-plugin",{text:"primer-plugin button",onAction:function(){n.setContent("<p>Hola Mundooo</p>")}})})}();
